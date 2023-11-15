88a07ff2d72d04018eb785ad31d07a0345a68322
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-11-15 19:59:28 +0100

    Add files via upload

--- class Player/player.py
+++ class Player/player.py
@@ -1,1 +1,14 @@
+class Player():
+    def __init__(self, name, age, rank):
+        self.name = name
+        self.age = age
+        self.rank = rank
 
+    def info(self):
+        print(f"Player: {self.name} \nAge: {self.age} \nRank: {self.rank}")
+
+player1 = Player("Neymar Jr", 31, 13)
+
+player2 = Player("Lionel Messi", 36, 4)
+
+player2.info()
