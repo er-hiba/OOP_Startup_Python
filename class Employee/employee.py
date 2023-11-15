a20ed655f300d1b140921f3bc82054a5dcb58af1
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-11-15 20:18:04 +0100

    :)

--- class Employee/employee.py
+++ class Employee/employee.py
@@ -6,9 +6,9 @@ class Employee() :
         self.base_salary = base_salary
 
     def info(self):
-        print(f"The employee {self.name} made {self.sales} sales, worked {self.bonus_hrs} bonus hours, \
-and \ntheir base salary is {self.base_salary}.")
+        print(f"The employee {self.name} made {self.sales} sales, worked {self.bonus_hrs} bonus hours, and \ntheir base salary is {self.base_salary}.")
 
+# Main Program
 emp1 = Employee("Joey", 500, 16, 2000)
 emp2 = Employee("Mohamed", 1000, 20, 4000)
 
