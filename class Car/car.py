07770900aad8216a8caad39d52fee6b73509c97b
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-11-15 20:16:55 +0100

    :)

--- class Car/car.py
+++ class Car/car.py
@@ -9,11 +9,10 @@ class Car():
         self.number = number
 
     def info(self):
-        print(f"    Car {self.number}: \nName: {self.name} \nModel: {self.model} \nSpeed: \
-{self.speed}km/h \nEngine power: {self.engine_power}hp \nColor: \
-{self.color} \nNitro Speed: {self.nitro_speed}" )
+        print(f"    Car {self.number}: \nName: {self.name} \nModel: {self.model} \nSpeed: {self.speed}km/h \nEngine power: \
+{self.engine_power}hp \nColor: {self.color} \nNitro Speed: {self.nitro_speed}" )
 
-
+# Main Program
 Car1 = Car("Ford Mustang", "GT", 250, "Black", "Yes", 450, 1)
 
 Car2 = Car("AUDI Q7", "Q7", 250, "Navarra Blue", "No", 335, 2)
